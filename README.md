40b07c1d7fbad568dce90ad9d82056351eef7180
Chandler <chndlr.wllm@gmail.com>
2017-10-08 15:12:55 -0500

    all functions working properly

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # liri-node-app
+
+## This Language Interpretation and Recognition Interface (LIRI) is a command line node app that takes in parameters and returns data.
--- liri.js
+++ liri.js
@@ -19,7 +19,6 @@ if (process.argv[2] === "my-tweets") {
         console.log(tweets[i].text);
         console.log(tweets[i].created_at);
       }
-        // console.log(tweets);
     }
   });
 }
@@ -56,7 +55,7 @@ else if (process.argv[2] === "spotify-this-song") {
 
   else {
   
-    var song = "'The Sign'";
+    var song = "'The Sign ace of base'";
     
     var spotify = new Spotify({
       id: llaves.spotifyKeys.client_id,
@@ -83,29 +82,53 @@ else if (process.argv[2] === "spotify-this-song") {
 
 else if (process.argv[2] === "movie-this") {
 
-  var movieName = "";
-  
-  for (var p = 3; p < process.argv.length; p++){
-    movieName += " " + process.argv[p];
-  };
-  
-  var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-  // console.log(queryURL);
+  if (process.argv[3]){
 
-  request(queryURL, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      // console.log(JSON.parse(body, null, 2));
-      console.log("Title: " + JSON.parse(body).Title);      
-      console.log("Release Year: " + JSON.parse(body).Year);
-      console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
-      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[0].Value);
-      console.log("Country: " + JSON.parse(body).Country);
-      console.log("Language: " + JSON.parse(body).Language);
-      console.log("Plot: " + JSON.parse(body).Plot);
-      console.log("Actors: " + JSON.parse(body).Actors);
-    }
-  })
+    var movieName = "";
+    
+    for (var p = 3; p < process.argv.length; p++){
+      movieName += " " + process.argv[p];
+    };
+    
+    var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+    // console.log(queryURL);
 
+    request(queryURL, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        // console.log(JSON.parse(body, null, 2));
+        console.log("Title: " + JSON.parse(body).Title);      
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Country: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+      }
+    })
+  }
+
+  else {
+
+    var movieName = "Mr. Nobody";
+    
+    var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+    // console.log(queryURL);
+
+    request(queryURL, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        // console.log(JSON.parse(body, null, 2));
+        console.log("Title: " + JSON.parse(body).Title);      
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Country: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+      }
+    })
+  }
 }
 
 
@@ -113,13 +136,35 @@ else if (process.argv[2] === "do-what-it-says") {
 
   fs.readFile("random.txt", "utf8", function(error, data) {
     
+    var doIt = data.split(",");
+
     if (error) {
       return console.log(error);
     }
+
+    else {
     
-    console.log("node liri.js " + data);
+      var spotify = new Spotify({
+        id: llaves.spotifyKeys.client_id,
+        secret: llaves.spotifyKeys.client_secret
+      });
+      
+      spotify.search({ type: 'track', query: doIt[1] }, function(error, data) {
+        if (error) {
+          return console.log('Error occurred: ' + error);
+        }
+      
+        else {
+          console.log("Artist: " + data.tracks.items[0].artists[0].name);
+          console.log("Track: " + data.tracks.items[0].name);
+          console.log("Album: " + data.tracks.items[0].album.name);  
+          console.log("Preview: " + data.tracks.items[0].preview_url);
+        }
+
+      })
     
-  });
+    }
+  })
 }
 
 else {
